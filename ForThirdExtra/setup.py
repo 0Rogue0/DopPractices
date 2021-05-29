77b525a630ca21ef1e7cdc6d1ae235b25832321e
0Rogue0 <Altair1-3@yandex.ru>
2021-05-29 16:52:46 +0300

    ExtraPractices

---
+++ ForThirdExtra/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="study_package",
+    version="0.0.1",
+    author="Huseyn",
+    author_email="ertek.k.i@edu.mirea.ru",
+    packages=["study_package"],
+    description="This is a test package for python practice"
+)
